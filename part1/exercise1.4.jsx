e6694a64c3cc003e99724dcc60c36cc1b223e832
090221979 <69111078+090221979@users.noreply.github.com>
2026-08-17 23:33:48 +0100

    put the objects in an array

---
+++ part1/exercise1.4.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+function Header({course})
+{
+  return <h1>{course}</h1>
+}
+
+const Part = (props)=>
+    {
+      return<p>{props.name} {props.exercise}</p>
+    }
+
+function Content({parts})
+{
+
+  return(
+    <div>
+        <Part name={parts[0].name} exercise={parts[0].exercises}/>
+      
+        <Part name={parts[1].name} exercise={parts[1].exercises}/>
+   
+        <Part name={parts[2].name} exercise={parts[2].exercises}/>
+
+    </div>
+  )
+}
+
+
+function Total({parts})
+{
+  return  <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+
+}
+
+export default function App()
+{
+ const course = 'Half Stack application development'
+ const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+  
+  return(
+    <div>
+       <Header course={course}/>
+       <Content parts={parts}/>
+       <Total parts={parts}/>
+    </div>
+  )
+}
