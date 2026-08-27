44e7d07c08b3d5724cce13fe76cba5a665f64b57
090221979 <69111078+090221979@users.noreply.github.com>
2026-08-27 16:01:52 +0100

    Add App component with course data and rendering

---
+++ part2/exercise2.4.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import './style.css';
+import Course from './coponents/courseComponent';
+
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
+
+  return (
+    <div>
+      <h2>Dummy Data</h2>
+
+      {courses.map((singleItem) => {
+        return <Course course={singleItem} key={singleItem.id} />;
+      })}
+    </div>
+  );
+};
+
+export default App;
